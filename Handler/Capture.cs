29e278080ac3f41a3a22a75206f537449a11d83a
Fredericklamouth <104854975+Fredericklamouth@users.noreply.github.com>
2024-04-10 14:00:26 -0400

    Add files via upload

---
+++ Handler/Capture.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea4SELENIUMN.Handler
+{
+    public class Capture
+    {
+        public static string Captura(IWebDriver driver, string carpeta, string nombre)
+        {
+            string RutaImagen = "..\\..\\ScrnShot\\" + carpeta + "\\" + nombre + ".png";
+            Screenshot imagen = ((ITakesScreenshot)driver).GetScreenshot();
+            imagen.SaveAsFile(RutaImagen);
+
+            return RutaImagen;
+        }
+    }
+}
---
+++ Handler/Reporte.cs
@@ -0,0 +1,27 @@
+﻿using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea4SELENIUMN.Handler
+{
+    public class Reporte
+    {
+        private static ExtentReports reporte;
+        private static ExtentSparkReporter ReporteHTML;
+        public static ExtentReports ObtenerReporte()
+        {
+            if (reporte == null)
+            {
+                string Ruta = @"C:\Users\frede\source\repos\Tarea4SELENIUMN\Reporte\Reporte.html";
+                ReporteHTML = new ExtentSparkReporter(Ruta);
+                reporte = new ExtentReports();
+                reporte.AttachReporter(ReporteHTML);
+            }
+            return reporte;
+        }
+    }
+}
